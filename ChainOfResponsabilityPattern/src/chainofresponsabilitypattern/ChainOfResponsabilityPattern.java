4f9b798126fcc88c47e5ddfc0a1521269eed00bd
Elvis <elvismariel@gmail.com>
2019-01-16 15:41:12 -0200

    Design Pattern: Chain of responsability

---
+++ ChainOfResponsabilityPattern/src/chainofresponsabilitypattern/ChainOfResponsabilityPattern.java
@@ -0,0 +1,31 @@
+package chainofresponsabilitypattern;
+
+import logger.AbstractLogger;
+import logger.ConsoleLogger;
+import logger.ErrorLogger;
+import logger.FileLogger;
+
+/**
+ *
+ * @author elvis
+ */
+public class ChainOfResponsabilityPattern {
+
+    private static AbstractLogger getChainOfLoggers() {
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+        loggerChain.logMessage(AbstractLogger.DEBUG, "This is an debug level information.");
+        loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+    }
+}
