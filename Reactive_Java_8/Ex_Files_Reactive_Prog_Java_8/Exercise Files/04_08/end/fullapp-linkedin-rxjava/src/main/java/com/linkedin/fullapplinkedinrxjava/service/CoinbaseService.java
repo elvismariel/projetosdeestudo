65997606fe6650695d9c9d4f5d8169743cc90f34
Elvis <elvismariel@gmail.com>
2019-01-25 20:09:17 -0200

    Reactive - remove exercises

--- Reactive_Java_8/Ex_Files_Reactive_Prog_Java_8/Exercise Files/04_08/end/fullapp-linkedin-rxjava/src/main/java/com/linkedin/fullapplinkedinrxjava/service/CoinbaseService.java
+++
@@ -1,9 +0,0 @@
-package com.linkedin.fullapplinkedinrxjava.service;
-
-import com.linkedin.fullapplinkedinrxjava.model.CoinBaseResponse;
-import reactor.core.publisher.Mono;
-
-public interface CoinbaseService {
-
-    Mono<CoinBaseResponse> getCryptoPrice(String priceName);
-}
